db8fe8e851ce02ad278a7f620e99687c5f6e4ebe
zaqai <17513614598@163.com>
2022-10-02 00:11:34 +0800

    modified2

--- server/storage/common.go
+++ server/storage/common.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"time"
 )
 
 // Storage is the interface for storage operation
@@ -14,13 +13,11 @@ type Storage interface {
 	// Head retrieves content length of a file from storage
 	Head(ctx context.Context, filename string) (contentLength uint64, err error)
 	// Put saves a file on storage
-	Put(ctx context.Context, filename string, reader io.Reader, contentType string, contentLength uint64) error
+	Put(ctx context.Context, filename string, reader io.Reader) error
 	// Delete removes a file from storage
 	Delete(ctx context.Context, filename string) error
 	// IsNotExist indicates if a file doesn't exist on storage
 	IsNotExist(err error) bool
-	// Purge cleans up the storage
-	Purge(ctx context.Context, days time.Duration) error
 
 	// Type returns the storage type
 	Type() string
